1077edf89da44f5d387700e02ed4ee8a1ab9fb84
ryumodrn <rjm9303@gmail.com>
2022-07-11 17:51:07 +0900

    feat($mvc-mapping): map path-variable into model-attribute

---
+++ spring-playground/spring-anything/src/main/java/com/springanything/mapping/RequestParamTestController.java
@@ -0,0 +1,25 @@
+package com.springanything.mapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class RequestParamTestController {
+
+	@GetMapping("/test/request-param")
+	public TestRequest mapping(@ModelAttribute TestRequest testRequest) {
+		log.info("testRequest = {}", testRequest);
+		return testRequest;
+	}
+
+	@GetMapping("/test/request-param/{name}")
+	public TestRequest mappingPathVariable(@ModelAttribute TestRequest testRequest) {
+		log.info("testRequest = {}", testRequest);
+		return testRequest;
+	}
+
+}
