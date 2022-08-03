19a119d85b536cd8dffa28bd04e661f8bbfd3f15
ryumodrn <rjm9303@gmail.com>
2022-08-03 16:14:50 +0900

    feat($mvc): ModelAttribute auto including should not be reassigned

--- spring-playground/spring-anything/src/main/java/com/springanything/mvc/ModelAttributeController.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/mvc/ModelAttributeController.java
@@ -10,13 +10,24 @@ import com.springanything.mapping.TestRequest;
 public class ModelAttributeController {
 
 	/**
-	 * ModelAttribute로 받아올 시 별도로 Model에 담아줄 필요 없음<br/>
-	 * html 보낼 때 자동으로 담아서 보내준다
+	 * ModelAttribute 매핑은 Model에 별도로 추가하지 않아도 자동으로 추가된다<br/>
+	 * Repository에서 반환 받은 값을 Model로 보내려면<br/>
+	 * 기존의 @ModelAttribute로 매핑 받는 객체에 재할당을 할 순 없고 상태의 변경이 필요하다<br/>
+	 * 이 때 사용하기 위한 API<br/>
+	 * <pre>{@code vo = repository.get(vo) 불가
+	 * vo.set(repository.get(vo)) 가능}</pre>
 	 */
 	@GetMapping("/model-attribute")
 	public String autoMapping(@ModelAttribute TestRequest request) {
-		request.setTestInnerRequest(new TestRequest.TestInnerRequest("yahoo~!~!~!"));
+		// request = getTestRequest(request); -> 불가, 값 매핑되지 않음
+
+		TestRequest result = getTestRequest(request);
+		request.setTestInnerRequest(result.getTestInnerRequest());
 		return "test-view";
+	}
+
+	private TestRequest getTestRequest(TestRequest request) {
+		return new TestRequest(request.getName(), request.getAge(), new TestRequest.TestInnerRequest("yaho!!"));
 	}
 
 }
