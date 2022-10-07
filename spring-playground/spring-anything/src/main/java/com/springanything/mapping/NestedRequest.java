fb2cbc1500ad396b7d45e815a87b8f9bc49c4473
ryumodrn <rjm9303@gmail.com>
2022-10-07 20:43:04 +0900

    feat($mapping): edit inner-request description

--- spring-playground/spring-anything/src/main/java/com/springanything/mapping/NestedRequest.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/mapping/NestedRequest.java
@@ -34,13 +34,12 @@ public class NestedRequest implements Serializable {
 중첩 객체의 경우, 선택이 필요하다
 팀의 컨벤션을 따르거나 형태의 전체적인 통일성을 맞추도록 리팩토링해야 한다
 
-1. @Setter 없애고 각각의 필드를 상위 객체의 생성자로 받는다
-- 상위 객체 @NoArgsConstructor(access = AccessLevel.PROTECTED), @Getter
-- 하위 객체 @NoArgsConstructor(access = AccessLevel.PROTECTED), @AllArgsConstructor(access = AccessLevel.PRIVATE), @Getter
+1. @Setter 없애고 각각의 필드를 Outer 객체의 생성자로 받는다
+- Outer 객체 @NoArgsConstructor(access = AccessLevel.PROTECTED), @AllArgsConstructor, @Getter
+- Inner 객체 @NoArgsConstructor(access = AccessLevel.PROTECTED), @AllArgsConstructor(access = AccessLevel.PRIVATE), @Getter
 이때 Controller에서 query-string ModelAttribute로 매핑한다면 아래와 같은 형태로 된다
 /test/reiterative?name=panda&nickname=bear
-즉 중첩 객체지만 innerRequest.name=xxx, innerRequest.nickname=xxx 형태로 매핑하지 않고
-name=xxx, nickname=xxx 형태로 매핑한다
+즉 중첩 객체지만 innerRequest.name=xxx, innerRequest.nickname=xxx 형태로 매핑하지 않고 name=xxx, nickname=xxx 형태로 매핑한다
 
 2. @NoArgs + @Setter 사용
 - @NoArgsConstructor(access = AccessLevel.PROTECTED), @Getter, @Setter
