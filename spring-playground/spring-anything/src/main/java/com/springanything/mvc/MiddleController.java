5dd7421622e37fef5c38e6ca107e22a92a068fc5
ryumodrn <rjm9303@gmail.com>
2022-11-26 22:31:27 +0900

    feat($mvc): add path-variable mapping in the middle of uri

---
+++ spring-playground/spring-anything/src/main/java/com/springanything/mvc/MiddleController.java
@@ -0,0 +1,15 @@
+package com.springanything.mvc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MiddleController {
+
+	/* uri 중간에 매핑해도 오께이 */
+	@GetMapping("/test/{vdiAgentId}/panda")
+	public String panda(@PathVariable String vdiAgentId) {
+		return "panda-%s".formatted(vdiAgentId);
+	}
+}
