11591e427b3c21447ae827ad560aaacd08adc763
ryumodrn <rjm9303@gmail.com>
2022-12-06 20:50:31 +0900

    feat(mvc): config bean with canonical name

---
+++ spring-playground/spring-anything/src/test/java/com/springanything/mvc/ConfigBeanTest.java
@@ -0,0 +1,47 @@
+package com.springanything.mvc;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest(classes = MainConfig.class)
+@Slf4j
+class ConfigBeanTest {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@DisplayName("@Configuration 설정 + 빈 이름 지정 하지 않을 시 canonical name으로 생성")
+	@Test
+	void beanName() {
+		// given, when
+		assertThatThrownBy(() -> context.getBean("configBean"))
+			.isInstanceOf(NoSuchBeanDefinitionException.class);
+	}
+}
+
+/*
+ConfigBean
+@Configuration을 통해 빈으로 등록해야 하는데 ComponentScan Filter에 의해 빈으로 등록되지 않음
+
+DelegateConfig
+@Import를 통해 ConfigBean을 등록,
+DelegateConfig은 Configuration이므로 ComponentScan Filter에 의해 빈으로 등록되지 않음
+
+MainConfig
+기본 설정 클래스, @ComponentScan을 통해 @Configuration을 제외한 모든 클래스를 스캔하여 빈으로 등록
+
+위와 같은 조건을 만족할 시
+org.springframework.beans.factory.NoUniqueBeanDefinitionException:
+  No qualifying bean of type 'com.springanything.mvc.AbstractBean' available:
+  expected single matching bean but found 2: concreteBean,com.springanything.mvc.ConfigBean
+
+ConfigBean이 configBean으로 등록되지 않고 canonical name으로 등록되어 발생하는 문제
+ */
