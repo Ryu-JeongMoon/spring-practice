699e8fa29b89f14afed4f09854409bd4d9e28150
ryumodrn <rjm9303@gmail.com>
2022-12-08 23:33:33 +0900

    feat(mvc): add abstract-mapping

--- spring-playground/spring-anything/src/main/java/com/springanything/mvc/ConcreteController.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/mvc/ConcreteController.java
@@ -1,8 +1,12 @@
 package com.springanything.mvc;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/concrete")
 public class ConcreteController extends AbstractController {
 
+	// /concrete/test/abstract ok~!
+	// 구체 클래스에서 지정해둔 Mapping이 먼저 적용
 }
