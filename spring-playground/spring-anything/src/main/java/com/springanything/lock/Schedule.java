4585a7fbdd0a511729816d39ec79a9d8b6e6fcf2
ryumodrn <rjm9303@gmail.com>
2023-04-27 16:31:43 +0900

    feat(lock): unlock by api

--- spring-playground/spring-anything/src/main/java/com/springanything/lock/Schedule.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/lock/Schedule.java
@@ -2,41 +2,41 @@ package com.springanything.lock;
 
 import java.util.Date;
 
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// @Component
+@Component
 public class Schedule {
 
-	@Scheduled(cron = "0/3 * * * * *", zone = "Asia/Seoul")
-	@SchedulerLock(name = "run1", lockAtLeastFor = "1s", lockAtMostFor = "3s")
-	void run1() throws InterruptedException {
-		Thread.sleep(2500);
-		log.info("Schedule.run1 at {}", new Date());
-	}
+  // @Scheduled(cron = "0/3 * * * * *", zone = "Asia/Seoul")
+  @SchedulerLock(name = "run1", lockAtLeastFor = "1s", lockAtMostFor = "3s")
+  void run1() throws InterruptedException {
+    Thread.sleep(2500);
+    log.info("Schedule.run1 at {}", new Date());
+  }
 
-	@SchedulerLock(name = "run1", lockAtLeastFor = "30s", lockAtMostFor = "30s")
-	void run2() {
-		log.info("Schedule.run2 at {}", new Date());
-	}
+  @SchedulerLock(name = "run1", lockAtLeastFor = "30m", lockAtMostFor = "30m")
+  void run2() {
+    log.info("Schedule.run2 at {}", new Date());
+  }
 
-	@Scheduled(cron = "0/3 * * * * *", zone = "Asia/Seoul")
-	@SchedulerLock(name = "run1", lockAtLeastFor = "1s", lockAtMostFor = "3s")
-	void run3() throws InterruptedException {
-		Thread.sleep(2500);
-		log.info("Schedule.run3 at {}", new Date());
-	}
+  // @Scheduled(cron = "0/3 * * * * *", zone = "Asia/Seoul")
+  @SchedulerLock(name = "run1", lockAtLeastFor = "1s", lockAtMostFor = "3s")
+  void run3() throws InterruptedException {
+    Thread.sleep(2500);
+    log.info("Schedule.run3 at {}", new Date());
+  }
 
-	@Scheduled(cron = "0/3 * * * * *", zone = "Asia/Seoul")
-	@SchedulerLock(name = "run1", lockAtLeastFor = "1s", lockAtMostFor = "3s")
-	void run4() throws InterruptedException {
-		Thread.sleep(2500);
-		log.info("Schedule.run4 at {}", new Date());
-	}
+  // @Scheduled(cron = "0/3 * * * * *", zone = "Asia/Seoul")
+  @SchedulerLock(name = "run1", lockAtLeastFor = "1s", lockAtMostFor = "3s")
+  void run4() throws InterruptedException {
+    Thread.sleep(2500);
+    log.info("Schedule.run4 at {}", new Date());
+  }
 }
 
 /*
--- spring-playground/spring-anything/src/main/java/com/springanything/lock/ScheduleController.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/lock/ScheduleController.java
@@ -1,6 +1,8 @@
 package com.springanything.lock;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -9,12 +11,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ScheduleController {
 
-	// private final Schedule schedule;
+  private final Schedule schedule;
+  private final StringRedisTemplate stringRedisTemplate;
 
-	/* Lock과 상관 없이 응답은 바로 됨 */
-	@GetMapping("/test-schedule")
-	public String schedule() {
-		// schedule.run2();
-		return "ok";
-	}
+  /* Lock과 상관 없이 응답은 바로 됨 */
+  @GetMapping("/test-schedule")
+  public String schedule() {
+    schedule.run2();
+    return "ok";
+  }
+
+  @GetMapping("/test-unlock/{method}")
+  public String unlock(@PathVariable String method) {
+    String lockPrefix = "job-lock:default:";
+    stringRedisTemplate.delete(lockPrefix + method);
+    return "ok";
+  }
 }
