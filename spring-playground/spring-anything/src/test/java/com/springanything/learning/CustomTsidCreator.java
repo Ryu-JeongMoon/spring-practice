cf02a6d71deb2d35bea849c2a809c24521baab20
ryumodrn <rjm9303@gmail.com>
2023-05-02 23:37:22 +0900

    chore(uuid): add comment

--- spring-playground/spring-anything/src/test/java/com/springanything/learning/CustomTsidCreator.java
+++ spring-playground/spring-anything/src/test/java/com/springanything/learning/CustomTsidCreator.java
@@ -7,6 +7,7 @@ import com.github.f4b6a3.tsid.TsidCreator;
 class CustomTsidCreator {
 
   public String create() {
+    // 1ms 단위로 정확히 동일 && 1 / 62^3으로 중복 발생
     return TsidCreator.getTsid() + RandomString.make(3);
   }
 }
