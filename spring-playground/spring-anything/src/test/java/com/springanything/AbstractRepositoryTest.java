59c939b78fbc928e9b454c9d30a8a1b54f09fb9e
ryumodrn <rjm9303@gmail.com>
2023-07-25 16:13:06 +0900

    chore(p6spy): rename class
    - reformat code

--- spring-playground/spring-anything/src/test/java/com/springanything/AbstractRepositoryTest.java
+++ spring-playground/spring-anything/src/test/java/com/springanything/AbstractRepositoryTest.java
@@ -3,25 +3,25 @@ package com.springanything;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import com.springanything.config.p6spy.P6SpyConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import com.springanything.config.TestConfig;
+import com.springanything.config.p6spy.P6spyConfig;
 
 @DataJpaTest
-@Import({TestConfig.class, P6SpyConfig.class})
+@Import({ TestConfig.class, P6spyConfig.class })
 public abstract class AbstractRepositoryTest {
 
-	protected final Logger log = LoggerFactory.getLogger(getClass());
+  protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	@PersistenceContext
-	protected EntityManager entityManager;
+  @PersistenceContext
+  protected EntityManager entityManager;
 
-	protected void flushAndClear() {
-		entityManager.flush();
-		entityManager.clear();
-	}
+  protected void flushAndClear() {
+    entityManager.flush();
+    entityManager.clear();
+  }
 }
