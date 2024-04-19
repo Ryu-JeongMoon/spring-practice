1a71e075fda025e804801448550f8f2e33745e70
ryumodrn <rjm9303@gmail.com>
2024-04-19 22:23:34 +0900

    feat(cache): apply redis auto-configuration

--- spring-playground/spring-anything/src/main/java/com/springanything/cache/CacheConfig.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/cache/CacheConfig.java
@@ -1,52 +1,28 @@
 package com.springanything.cache;
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
-@RequiredArgsConstructor
 public class CacheConfig {
 
-  private final RedisProperties redisProperties;
-
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return jedis();
-  }
-
-  private RedisConnectionFactory jedis() {
-    RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
-    JedisClientConfiguration clientConfig = JedisClientConfiguration
-      .builder()
-      .usePooling()
-      .build();
-    return new JedisConnectionFactory(redisStandaloneConfig, clientConfig);
-  }
-
-  // public RedisConnectionFactory lettuce() {
-  // 	RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
-  // 	configuration.setPassword(RedisPassword.none());
-  // 	return new LettuceConnectionFactory(configuration);
-  // }
-
-  @Bean
-  public StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    StringRedisTemplate stringRedisTemplate = new StringRedisTemplate(redisConnectionFactory);
-    stringRedisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
-    return stringRedisTemplate;
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+    return redisTemplate;
   }
 
   @Bean
