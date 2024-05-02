308dd238fb06fa74e63f379637da6cad8dbb8055
ryumodrn <rjm9303@gmail.com>
2024-05-02 22:22:54 +0900

    feat(lock): add redisson lock test

--- spring-playground/spring-anything/src/main/java/com/springanything/cache/CacheConfig.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/cache/CacheConfig.java
@@ -1,5 +1,12 @@
 package com.springanything.cache;
 
+import java.util.Optional;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +16,14 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+
+  private static final String REDIS_PROTOCOL_PREFIX = "redis://";
+  private static final String REDISS_PROTOCOL_PREFIX = "rediss://";
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -28,5 +39,29 @@ public class CacheConfig {
   @Bean
   public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
     return RedisCacheManager.builder(redisConnectionFactory).build();
+  }
+
+  @Bean
+  public RedissonClient redissonClient(RedisProperties redisProperties) {
+    String prefix = redisProperties.getSsl().isEnabled()
+      ? REDISS_PROTOCOL_PREFIX
+      : REDIS_PROTOCOL_PREFIX;
+
+    int timeout = Optional.ofNullable(redisProperties.getTimeout())
+      .map(x -> (int) x.toMillis())
+      .orElse(10000);
+
+    Config config = new Config();
+    SingleServerConfig singleServerConfig = config.useSingleServer();
+
+    singleServerConfig.setAddress(prefix + redisProperties.getHost() + ":" + redisProperties.getPort())
+      .setConnectTimeout(timeout)
+      .setDatabase(redisProperties.getDatabase());
+
+    if (StringUtils.hasText(redisProperties.getPassword())) {
+      singleServerConfig.setPassword(redisProperties.getPassword());
+    }
+
+    return Redisson.create(config);
   }
 }
---
+++ spring-playground/spring-anything/src/main/java/com/springanything/cache/RedissonController.java
@@ -0,0 +1,38 @@
+package com.springanything.cache;
+
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class RedissonController {
+
+  private final RedissonClient redisson;
+
+  @GetMapping("/test/redisson/{id}")
+  public String get(@PathVariable String id) {
+    RLock lock = redisson.getLock("test");
+    try {
+      boolean acquired = lock.tryLock(10, TimeUnit.SECONDS);
+      if (acquired) {
+        log.info("lock acquired : {}", id);
+        Thread.sleep(2000);
+      }
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } finally {
+      lock.unlock();
+    }
+
+    return "hello world!";
+  }
+}
