db9ee37cbd9a97e3941f75f8cfa53b80519505ee
ryumodrn <rjm9303@gmail.com>
2024-10-18 16:28:00 +0900

    feat(validation): ignore nested validation

--- spring-playground/spring-anything/src/main/java/com/springanything/mapping/validation/nested/NestedValidationController.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/mapping/validation/nested/NestedValidationController.java
@@ -13,4 +13,9 @@ public class NestedValidationController {
   public OuterNestedRequest testNestedValidation(@Valid @RequestBody OuterNestedRequest request) {
     return request;
   }
+
+  @PostMapping("/test/nested-validation/v2")
+  public OuterNestedRequest testNestedValidationV2(@RequestBody OuterNestedRequest request) {
+    return request;
+  }
 }
