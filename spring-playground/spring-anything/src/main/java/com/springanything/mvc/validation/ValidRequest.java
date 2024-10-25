58d95b9fc0f47f05a037281fed10601866a9b95d
ryumodrn <rjm9303@gmail.com>
2024-10-25 17:12:49 +0900

    feat(validation): add URL validation

--- spring-playground/spring-anything/src/main/java/com/springanything/mvc/validation/ValidRequest.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/mvc/validation/ValidRequest.java
@@ -3,9 +3,15 @@ package com.springanything.mvc.validation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.URL;
+
 public record ValidRequest(
-  @Size(max = 255) String boundedInput,
-  @NotBlank String unboundedInput
+  @Size(max = 255)
+  String boundedInput,
+  @NotBlank
+  String unboundedInput,
+  @URL
+  String url
 ) {
 
 }
