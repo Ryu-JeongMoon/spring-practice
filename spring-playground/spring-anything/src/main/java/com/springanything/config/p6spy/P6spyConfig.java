405d60bad4b28b45a96b8d57b84b8fbcc6ce07ce
ryumodrn <rjm9303@gmail.com>
2024-10-28 21:59:33 +0900

    refactor(p6spy): add condition

--- spring-playground/spring-anything/src/main/java/com/springanything/config/p6spy/P6spyConfig.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/config/p6spy/P6spyConfig.java
@@ -2,11 +2,13 @@ package com.springanything.config.p6spy;
 
 import jakarta.annotation.PostConstruct;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 import com.p6spy.engine.spy.P6SpyOptions;
 
 @Configuration
+@ConditionalOnProperty(value = "conditional.enabled.jta", havingValue = "false", matchIfMissing = true)
 public class P6spyConfig {
 
   @PostConstruct
