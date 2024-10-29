06a0179dd9cf69bcedcab3202af032fa5abc267c
ryumodrn <rjm9303@gmail.com>
2024-10-29 20:16:24 +0900

    refactor(jpa): User Entity & Repository

--- spring-playground/spring-anything/src/main/java/com/springanything/jpa/nplus1/User.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/jpa/nplus1/User.java
@@ -2,7 +2,6 @@ package com.springanything.jpa.nplus1;
 
 import java.util.HashSet;
 import java.util.Set;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -16,20 +15,24 @@ import org.hibernate.annotations.BatchSize;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+@ToString(exclude = {"articles", "questions"})
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  // @BatchSize(size = 100)
+  @BatchSize(size = 100)
   @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
   private Set<Article> articles = new HashSet<>();
   // private List<Article> articles = new ArrayList<>();
@@ -40,15 +43,19 @@ public class User {
   // private List<Question> questions = new ArrayList<>();
 
   @Column(length = 10, nullable = false)
-  private String name;
+  private String firstName;
 
-  public User(String name) {
-    this.name = name;
-  }
+  @Column(length = 10, nullable = false)
+  private String secondName;
 
   @Builder
-  public User(Long id, String name) {
+  public User(
+    Long id,
+    String firstName,
+    String secondName
+  ) {
     this.id = id;
-    this.name = name;
+    this.firstName = firstName;
+    this.secondName = secondName;
   }
 }
--- spring-playground/spring-anything/src/main/java/com/springanything/jpa/nplus1/UserRepository.java
+++ spring-playground/spring-anything/src/main/java/com/springanything/jpa/nplus1/UserRepository.java
@@ -10,22 +10,25 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  @Query("select u from User u left join u.articles where u.name = :name")
-  List<User> findByNameJPQL(String name);
+  @Query("select u from User u left join u.articles where u.firstName = :firstName")
+  List<User> findByFirstNameJPQL(String firstName);
 
-  @Query("select distinct u from User u left join u.articles")
+  @Query("select u from User u left join u.articles")
   List<User> findAllJPQL();
 
-  @Query("select distinct u from User u left join fetch u.articles")
+  @Query("select u from User u left join fetch u.articles")
   List<User> findAllByJPQLFetch();
 
-  @EntityGraph(attributePaths = { "articles" }, type = EntityGraph.EntityGraphType.FETCH)
-  @Query("select distinct u from User u left join u.articles")
+  @EntityGraph(attributePaths = {"articles"}, type = EntityGraph.EntityGraphType.FETCH)
+  @Query("select u from User u left join u.articles")
   List<User> findAllByEntityGraph();
 
-  @EntityGraph(attributePaths = { "articles" }, type = EntityGraph.EntityGraphType.FETCH)
-  @Query("select distinct u from User u left join u.articles")
-  Page<User> findAllByPage(Pageable pageable);
+  @Query("select u from User u join fetch u.questions")
+  Page<User> findAllQuestionsPageByFetchJoin(Pageable pageable);
+
+  @EntityGraph(attributePaths = {"questions"}, type = EntityGraph.EntityGraphType.FETCH)
+  @Query("select u from User u left join u.questions")
+  Page<User> findAllQuestionsPageByEntityGraph(Pageable pageable);
 
   Page<User> findAll(Pageable pageable);
 
