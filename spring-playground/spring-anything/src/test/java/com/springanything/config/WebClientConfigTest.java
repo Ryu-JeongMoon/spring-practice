358a665093f047c21dcba4806fc47db0aee14b88
ryumodrn <rjm9303@gmail.com>
2024-11-02 13:20:39 +0900

    test: disable webclient test

--- spring-playground/spring-anything/src/test/java/com/springanything/config/WebClientConfigTest.java
+++ spring-playground/spring-anything/src/test/java/com/springanything/config/WebClientConfigTest.java
@@ -3,6 +3,7 @@ package com.springanything.config;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.springanything.AbstractIntegrationTest;
 import com.springanything.mapping.TestRequest;
 
+@Disabled
 class WebClientConfigTest extends AbstractIntegrationTest {
 
   @Autowired
