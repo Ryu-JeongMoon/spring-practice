c8c23b5bff995c8b19dd8907046d4135a8884c82
ryumodrn <rjm9303@gmail.com>
2024-11-05 22:16:20 +0900

    refactor(test): webflux test

--- spring-playground/spring-anything/src/test/java/com/springanything/webflux/WebFluxControllerTest.java
+++ spring-playground/spring-anything/src/test/java/com/springanything/webflux/WebFluxControllerTest.java
@@ -1,33 +1,23 @@
 package com.springanything.webflux;
 
-import jakarta.persistence.EntityManager;
-
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.Commit;
 
-import com.springanything.config.TestConfig;
+import com.springanything.AbstractIntegrationTest;
 import com.springanything.jpa.relationship.Agent;
 import com.springanything.jpa.relationship.AgentOption;
 import com.springanything.jpa.relationship.AgentRepository;
 
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
-@Slf4j
-@DataJpaTest
-@Import(TestConfig.class)
-class WebFluxControllerTest {
+@Disabled
+class WebFluxControllerTest extends AbstractIntegrationTest {
 
-  @Autowired
-  EntityManager em;
   @Autowired
   AgentRepository agentRepository;
 
-  @Commit
   @Test
   @DisplayName("Flux 안에서 blocking 코드도 문제 없다")
   void flux() throws InterruptedException {
